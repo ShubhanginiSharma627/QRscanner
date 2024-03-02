9ee463a2e6439da519eb1e1eda5db7d51065057c
ShubhanginiSharma627 <sharma546shubu@gmail.com>
2024-03-02 15:44:12 +0530

    commit

--- index.js
+++ index.js
@@ -5,5 +5,10 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-
+import { firebase } from '@react-native-firebase/app';
+firebase.initializeApp({
+    // ... other config
+    authDomain: "qrscanner-6edf4.firebaseapp.com",
+  });
+  
 AppRegistry.registerComponent(appName, () => App);
